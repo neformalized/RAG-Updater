5ad916f7734f5f0dbe6719c965036edf39165d38
neformalized <neformalized@gmail.com>
2026-01-14 22:08:55 +0200

    main

---
+++ handler.py
@@ -0,0 +1,34 @@
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+#
+
+@app.post("/ask")
+async def ask(item: str):
+	
+    return {
+        "status": "ok",
+        "response": "ok"
+    }
+#
+
+#
+
+@app.post("/put")
+async def put("/put"):
+    
+    return {
+        "status": "ok",
+        "response": "ok"
+    }
+#
+
+if __name__ == "__main__":
+    
+    uvicorn.run(
+        app,
+        host="127.0.0.1",
+        port=8000
+    )
